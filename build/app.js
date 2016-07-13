f7cc2659c88f32d6a42a6af1e5f3712827ecbf78
Jovansonlee Cesar <ivanceras@gmail.com>
2016-07-13 17:19:39 +0800

    forgot to build

--- build/app.js
+++ build/app.js
@@ -32,10 +32,20 @@ function alignScroll(event, table, column_shadow_id, row_shadow_id){
 
 }
 
+
+/// To hackers: If you are attempting to hack the hardcoded db_url
+/// nevermind doing it, this is just a mock database generated from mockaroo.com
+/// using the free tier of heroku db
+/// and a 5$ docker container in digitalocean.com
+
 function sendSettingsDbUrl(){
     var dbUrl = localStorage.getItem("db_url");
     if (!dbUrl){
-        dbUrl = "postgres://user:pwd@localhost:5432/db";
+        if (window.location.hostname == "curtain-elm.herokuapp.com"){ // the heroku app demo 
+            dbUrl = "postgres://atperknxxnjadk:jpasCIjuPd3MW48DUnb579-imU@ec2-23-21-140-156.compute-1.amazonaws.com:5432/dd2fbo2kj0q9l";
+        }else{
+            dbUrl = "postgres://postgres:p0stgr3s@localhost:5432/mock"; //self hosted demo
+        }
     }
     console.log("sending db_url", dbUrl);
     app.ports.receiveSettingsDbUrl.send(dbUrl.toString());
@@ -48,7 +58,11 @@ function saveSettingsDbUrl(dbUrl){
 function sendSettingsApiServer(){
     var apiServer = localStorage.getItem("api_server");
     if (!apiServer){
-        apiServer = "http://localhost:3224"
+        if (window.location.hostname == "curtain-elm.herokuapp.com"){ 
+            apiServer = "https://iron-curtain.herokuapp.com"
+        }else{
+            apiServer = "http://45.55.7.231:3224";
+        }
     }
     console.log("sending api_server", apiServer)
     app.ports.receiveSettingsApiServer.send(apiServer.toString());
