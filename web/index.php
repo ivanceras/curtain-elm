8e0b5f052322c71a9336c1993c24741ef19f41b8
Jovansonlee Cesar <ivanceras@gmail.com>
2017-01-12 20:24:29 +0800

    Added boomerang for heroku add-on

--- web/index.php
+++ web/index.php
@@ -1,4 +1,10 @@
 <?php 
 // allow heroku to detect it as php app while only serving static html
 include_once("index.html");
+<script src="https://s3.amazonaws.com/assets.heroku.com/boomerang/boomerang.js"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    Boomerang.init({app: 'foo', addon: 'bar'});
+  });
+</script>
 ?>
